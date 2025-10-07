7b5f283b0d259969b235132de653c3823c48c06b
Bozio96 <rodrigobozio96@gmail.com>
2025-10-07 16:31:36 -0300

    feat:Modelando clientes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,30 @@
-// src/app.module.ts
 import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppDataSource } from './database/data-source';
-import { ProductsModule } from './products/products.module';
+import { ClientsModule } from './clients/clients.module';
+import { DepartmentsModule } from './departments/departments.module';
+import { SuppliersModule } from './suppliers/suppliers.module';
 import { BrandsModule } from './brands/brands.module';
 import { CategoriesModule } from './categories/categories.module';
-import { SuppliersModule } from './suppliers/suppliers.module';
-import { DepartmentsModule } from './departments/departments.module';
-import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(AppDataSource.options),
-    ProductsModule,
-    BrandsModule,
-    CategoriesModule,
-    SuppliersModule,
-    DepartmentsModule,
     UsersModule,
     AuthModule,
+    ProductsModule,
+    CategoriesModule,
+    BrandsModule,
+    SuppliersModule,
+    DepartmentsModule,
+    ClientsModule,
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -6,7 +6,7 @@ export const AppDataSource = new DataSource({
   host: 'localhost',
   port: 3306,
   username: 'root', // 👈 Cambia si usas otro usuario
-  password: '1234', // 👈 Tu contraseña de MySQL
+  password: 'root', // 👈 Tu contraseña de MySQL
   database: 'inventario_db', // 👈 Nombre de la base de datos
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   synchronize: true, // ⚠️ Solo en desarrollo
