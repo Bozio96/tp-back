03369ef675b6c6cca29a6e205cf4836a22d9c0e7
Bozio96 <rodrigobozio96@gmail.com>
2025-10-14 17:26:21 -0300

    Previo a creacion de rama

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -4,12 +4,12 @@ import { DataSource } from 'typeorm';
 export const AppDataSource = new DataSource({
   type: 'mysql',
   host: 'localhost',
-  port: 3306,
-  username: 'root', // 👈 Cambia si usas otro usuario
-  password: 'root', // 👈 Tu contraseña de MySQL
-  database: 'inventario_db', // 👈 Nombre de la base de datos
+  port: 3306,  //Puerto de la BBDD
+  username: 'root', // Cambia si usas otro usuario
+  password: 'root', // Tu contraseña de MySQL
+  database: 'inventario_db', // Nombre de la base de datos
   entities: [__dirname + '/../**/*.entity{.ts,.js}'],
   synchronize: true, // ⚠️ Solo en desarrollo
-  logging: true,
+  //logging: true, //Comentar si no se quiere tanto SQL en consola
   timezone: 'Z',
 });
